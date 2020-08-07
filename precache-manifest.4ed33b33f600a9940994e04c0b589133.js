dcdcb0ad79f7bed38d934815cb1da21ec63fbe2e
e-lab innovations <mohammedashad23@gmail.com>
2020-08-07 09:47:53 +0530

    first commit

---
+++ precache-manifest.4ed33b33f600a9940994e04c0b589133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c8496d2d5a1468e21bc1",
+    "url": "/static/js/main.c8496d2d.chunk.js"
+  },
+  {
+    "revision": "16985a49e07a58f889f9",
+    "url": "/static/js/1.16985a49.chunk.js"
+  },
+  {
+    "revision": "c8496d2d5a1468e21bc1",
+    "url": "/static/css/main.76672235.chunk.css"
+  },
+  {
+    "revision": "0e09154d290d6cc75e52bc7e46e7fb31",
+    "url": "/index.html"
+  }
+];
